8948e18c2e9e8a91f684e764def4892ecee5ed44
Ariisu <ariisutyan@gmail.com>
2022-01-26 16:11:54 +0300

    done

---
+++ src/test/java/ann/blin/config/Project.java
@@ -0,0 +1,15 @@
+package ann.blin.config;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class Project {
+    public static ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
+
+    public static boolean isRemoteWebDriver() {
+        return !config.remoteBrowser().equals("");
+    }
+
+    public static boolean isVideoOn() {
+        return !config.videoStorage().equals("");
+    }
+}
